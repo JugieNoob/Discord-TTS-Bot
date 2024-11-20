0f0ae772f4c11f6a14c52c16f020de7333729b05
JugieNoob <jurgenarna@gmail.com>
2024-11-20 14:37:39 +0100

    TTS Channel Config

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ bot = commands.Bot(".", intents=discord.Intents.all(), help_command=None)
 async def on_ready():
     print(f'Logged in as {bot.user}')
     
-    
 
 
 @bot.command("tts")
@@ -21,8 +20,19 @@ async def self(ctx, msg:str):
     
     outputfile = gtts.gTTS(msg)
     outputfile.save("tts.mp3")
-    voice_client.play(discord.FFmpegPCMAudio(source="assets/tts.mp3"))
+    voice_client.play(discord.FFmpegPCMAudio(source="tts.mp3"))
+
+@bot.command("config")
+async def self(ctx, channel:discord.TextChannel):
+    try:
+        with open("channel.txt", "w") as file:
+                file.write(str(channel.id))
+                print("Wrote new channel id to channel.txt")
+                await ctx.send(f"Set TTS channel to <#{channel.id}>")
+    except:
+        await ctx.send("Could not set TTS channel. Did you specify a channel?")
 
 
+    
 load_dotenv()
 bot.run(os.getenv("TOKEN"))
